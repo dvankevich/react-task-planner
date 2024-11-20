d6033d95136ca4594e57726ecf2e87316b3b1b16
dvankevich <dvankevich@gmail.com>
2024-11-20 17:18:41 +0200

    query indicator

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchTasks } from "../../redux/operations";
 import { AppBar } from "../AppBar/AppBar";
 import { TaskForm } from "../TaskForm/TaskForm";
@@ -8,6 +8,8 @@ import css from "./App.module.css";
 
 export default function App() {
   const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.tasks.isLoading);
+  const error = useSelector((state) => state.tasks.error);
 
   useEffect(() => {
     dispatch(fetchTasks());
@@ -17,6 +19,7 @@ export default function App() {
     <div className={css.container}>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
     </div>
   );
