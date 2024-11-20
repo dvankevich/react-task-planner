439ba80c4287a2404ba13d2450f6f14d9018918a
dvankevich <dvankevich@gmail.com>
2024-11-20 17:15:59 +0200

    read tasks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,18 @@
-import { AppBar } from '../AppBar/AppBar';
-import { TaskForm } from '../TaskForm/TaskForm';
-import { TaskList } from '../TaskList/TaskList';
-import css from './App.module.css';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchTasks } from "../../redux/operations";
+import { AppBar } from "../AppBar/AppBar";
+import { TaskForm } from "../TaskForm/TaskForm";
+import { TaskList } from "../TaskList/TaskList";
+import css from "./App.module.css";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <AppBar />
